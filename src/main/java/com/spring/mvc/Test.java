16a643767cc08f3c6d43d362bd2b9f3214374116
unknown <aroradeep474@gmail.com>
2023-02-11 21:07:27 +0530

    Added new files to the project

---
+++ src/main/java/com/spring/mvc/Test.java
@@ -0,0 +1,17 @@
+package com.spring.mvc;
+
+public class Test {
+
+	public static void m1()
+	{
+		System.out.println("We added a file");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+       m1();
+	}
+
+}
